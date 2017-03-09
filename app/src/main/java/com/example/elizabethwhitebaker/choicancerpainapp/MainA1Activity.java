ce6c884962d56af986b424f2ba654d60c810f223
efrwhite <efrwhite@aol.com>
2017-03-09 00:35:40 -0500

    Added Patient Cancer Diagnosis view (View A2) - unfinished

---
+++ app/src/main/java/com/example/elizabethwhitebaker/choicancerpainapp/MainA1Activity.java
@@ -0,0 +1,13 @@
+package com.example.elizabethwhitebaker.choicancerpainapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainA1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_a1);
+    }
+}
---
+++ app/src/main/java/com/example/elizabethwhitebaker/choicancerpainapp/MainA8Activity.java
@@ -0,0 +1,13 @@
+package com.example.elizabethwhitebaker.choicancerpainapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainA8Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_a8);
+    }
+}
